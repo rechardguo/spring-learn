a865d0b502705ce81e3be3f6e4ed26f2ac33b7c5
reguo <guo_xiaolong@pactera.com>
2018-12-13 10:19:16 +0800

    替换system.out为logger

--- springboot/webmvc/src/main/java/rechard/learn/springboot/jms/handler/UserCreateJMSHandler.java
+++ springboot/webmvc/src/main/java/rechard/learn/springboot/jms/handler/UserCreateJMSHandler.java
@@ -1,12 +1,15 @@
 package rechard.learn.springboot.jms.handler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import rechard.learn.springboot.RequestType;
 import rechard.learn.springboot.entity.User;
 import rechard.learn.springboot.stereotype.JMSHandler;
 
 @JMSHandler(jmsPayLoad = User.class, requestType = RequestType.CREATE_USER)
 public class UserCreateJMSHandler implements JMSListenerHandler {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
     public void process(Object msg) throws Exception {
-        System.out.println("create user : "+msg.toString());
+        logger.info("create user : "+msg.toString());
     }
 }
